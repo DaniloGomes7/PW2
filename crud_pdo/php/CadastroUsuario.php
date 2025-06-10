d953fb9edf37909cfa19d26fd14ac9c3237e4131
DaniloGomes7 <danilo.gomes48@etec.sp.gov.br>
2025-06-09 21:59:12 -0300

    Crud Feito, implementação do Swith

--- crud_pdo/php/CadastroUsuario.php
+++ crud_pdo/php/CadastroUsuario.php
@@ -1,35 +1,36 @@
 <?php
-require_once 'Conexao.php';
+    require_once 'Conexao.php';
 
-$email = $_POST['emailFormulario'];
-$nome  = $_POST['nomeFormulario'];
-$senha = $_POST['senhaFormulario'];
+    $email = $_POST['emailFormulario'];
+    $nome  = $_POST['nomeFormulario'];
+    $senha = $_POST['senhaFormulario']; 
 
 
+    
+    if(!empty($email) && !empty($nome) && !empty($senha)){
+        //Pegamos a senha digitado pelo usuário e realizamos uma criptografia nela
+        //com base nisso, vamos armazenar o hash(criptografia) no banco de dados.
+        $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
 
-if(!empty($email) && !empty($nome) && !empty($senha)){
-    //pegamos a senha digitado pelo o usuario e realizamops uma 
-    //criptografia nela com base nisso, vamos armazenar o hash(criptografia) no banco de dados.
-    $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+        //Instrução DML
+        $sql = "INSERT INTO usuarios (email, nome, senha) VALUES (:email, :nome, :senha)";
 
-    //Instrucao DML
-    $sql = "INSERT INTO usuarios (email, nome, senha) VALUES (:email, :nome, :senha)";
+        //preparar a inserção de dados no banco
+        $requisicao = $conexao->prepare($sql);
 
-    //preparar a inserção de dados no banco
-    $requisicao = $conexao ->prepare($sql);
+        $requisicao->bindParam(':email', $email);
+        $requisicao->bindParam(':nome', $nome);
+        $requisicao->bindParam(':senha', $senhaHash);
 
-    $requisicao->bindParam(':email',$email);
-    $requisicao->bindParam(':nome',$nome);
-    $requisicao->bindParam(':senha',$senhaHash);
+        try{
+            $requisicao->execute();
+            echo 'Usuário cadastrado com sucesso!';
+        }catch(PDOException $e){
+            echo 'Erro ao cadastrar: ' . $e->getMessage();
+        }
 
-    try{
-        $requisicao->execute();
-        echo "Usuario cadastrado com sucesso!!";
-    }catch(PDOException $e){
-        echo "Erro ao cadastrar: " . $e->getMessage();
+    }else{
+        echo '<p style="color:red;">Preencha todos os campos. </p>';
     }
-}else{
-    echo'<p style="color:red;">Preencha todos os campos por favor!! </p>';
-}
 
 ?>
--- crud_pdo/php/Conexao.php
+++ crud_pdo/php/Conexao.php
@@ -1,32 +1,26 @@
 <?php
-//declarando as variaveis que vamos utilizar na conexão
-$host = 'localhost';
-$nomeBanco = 'crud_pdo';
-$usuario = 'root';
-$senha = '';
+    //declarando as variáveis que vamos utilizar na conexão
+    $host      = 'localhost';
+    $nomeBanco = 'crud_pdo';
+    $usuario   = 'root';
+    $senha     = '';
+    
 
-//Criar um novo objeto que conecta no banco de dados
-//Vamos utilizar o PDO para isso, passando as variaveis,
-//acima como parametro
-try {
-    $conexao = new PDO(
-        "mysql:host=$host;
-         dbname=$nomeBanco;charset=UTF8",
-        $usuario,
-        $senha
-    );
-    //Define como o PDO vai tratar o erro no caso, vai lançar
-    //uma exceção, que posteriormente sera tratada no CATCH
+    //Criar um novo objeto que conecta no banco de dados
+    //Vamos utilizar o PDO para isso, passando as variaveis acima como parametro. 
+    try{
+        $conexao = new PDO(
+            "mysql:host=$host;
+            dbname=$nomeBanco;charset=UTF8",
+            $usuario,
+            $senha
+        );
+        //Define como o PDO vai tratar o erro
+        //No caso, vai lançar uma exceção, que posteriormente será tratada no CATCH
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexão realizada com êxito! <br><hr><br>";
 
-    $conexao->setAttribute(PDO::ATTR_ERRMODE,
-        PDO::ERRMODE_EXCEPTION);
-    echo"Conexao realizada com Êxito!! <br><hr><br>";
-
-} catch (PDOException $e) {
-    echo "ERRO:" . $e->getMessage();        
-
-}
-
-
-
+    }catch(PDOException $e){
+        echo "ERRO: " . $e->getMessage();
+    }
 ?>
